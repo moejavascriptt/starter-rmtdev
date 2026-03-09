48b71264fd43f7f101c419ed29955b2fb86c8a36
Moe Stax <mauriceg27@yahoo.com>
2026-03-09 13:39:17 -0400

    starter code + minor changes to existing comp

--- src/components/JobItemContent.tsx
+++ src/components/JobItemContent.tsx
@@ -1,6 +1,10 @@
 import BookmarkIcon from "./BookmarkIcon";
 
 export default function JobItemContent() {
+
+  return <EmptyJobContent />
+
+
   return (
     <section className="job-details">
       <div>
