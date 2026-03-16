a0e85f62d44c0f67f13d46cf0969fac6bf4bb362
Moe Stax <mauriceg27@yahoo.com>
2026-03-15 23:39:14 -0400

    jobitem type //

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,9 +1,15 @@
+import { JobItem } from "../lib/types";
 import JobListItem from "./JobListItem";
 import Spinner from "./Spinner";
 
+type JobListProps = {
+  jobItems: JobItem[]
+  isLoading: boolean
+}
+
 export function JobList({
   jobItems, isLoading
-}) {
+}: JobListProps) {
   return <ul className="job-list">
     {isLoading && < Spinner />}
     {
