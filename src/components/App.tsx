11a8ad6f95a2959a905659084aacf8c617921ea6
Moe Stax <mauriceg27@yahoo.com>
2026-03-16 14:07:37 -0400

    usejobitems custom hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,28 +12,12 @@ import ResultsCount from "./ResultsCount";
 import SortingControls from "./SortingControls";
 import JobList from "./JobList";
 import PaginationControls from "./PaginationControls";
+import { useJobItems } from "../lib/hooks";
 
 function App() {
 
 const [searchText, setSearchText] = useState("")
-const [jobItems, setJobItems] = useState([])
-const [isLoading, setIsLoading] = useState(false)
-
-  useEffect(() => {
-    if (!searchText) return
-
-    const fetchData = async () => {
-      setIsLoading(true)
-      const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`)
-      const data = await response.json()
-      setIsLoading(false)
-      setJobItems(data.jobItems)
-    }
-
-    fetchData()
-  }, [searchText])
-
+const {jobItems, isLoading } = useJobItems(searchText)
 
   return (
   <>
